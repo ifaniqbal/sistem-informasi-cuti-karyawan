410e82439e0d8df5bb3b66ab9a79be619f4e206c
Ifan Iqbal <ifaniqbal.com@gmail.com>
2021-05-28 13:53:50 +0700

    Add Spatie Laravel Permission. Update DatabaseSeeder with roles.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        if (! Role::count()) {
+            Role::create(['name' => 'Admin']);
+            Role::create(['name' => 'HR']);
+            Role::create(['name' => 'Employee']);
+        }
+
+        if (! User::where('email', 'admin@gmail.com')->count()) {
+            User::create([
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('password'),
+            ])->assignRole('Admin');
+        }
+
+        if (! User::where('email', 'hr@gmail.com')->count()) {
+            User::create([
+                'name' => 'HR',
+                'email' => 'hr@gmail.com',
+                'password' => bcrypt('password'),
+            ])->assignRole('HR');
+        }
+
+        if (! User::where('email', 'employee@gmail.com')->count()) {
+            User::create([
+                'name' => 'Employee',
+                'email' => 'employee@gmail.com',
+                'password' => bcrypt('password'),
+            ])->assignRole('Employee');
+        }
     }
 }
