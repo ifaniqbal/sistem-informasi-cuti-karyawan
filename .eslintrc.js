017cf7640962888379eba4023895d835a18d9933
Ifan Iqbal <ifaniqbal.com@gmail.com>
2021-05-28 13:43:08 +0700

    Add starter kit. - Laravel 8 - Laravel Jetstream - ESlint - PHP CS Fixer

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: [
+    'plugin:vue/vue3-recommended',
+  ],
+  rules: {
+    "indent": ["error", 2],
+    "vue/html-closing-bracket-newline": ["error", {
+      "singleline": "never",
+      "multiline": "never"
+    }],
+    "vue/no-v-html": "off",
+    "vue/require-prop-types": "off",
+    "vue/require-default-prop": "off",
+    "vue/component-tags-order": "off",
+    "vue/no-arrow-functions-in-watch": "off",
+    "vue/script-indent": ["error", 2, { "baseIndent": 0 }]
+  }
+}
